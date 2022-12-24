3cd04aa01dbb31987212caefa5fdb717ae3a1c88
Nikhil <Sknikhil2000@gmail.com>
2022-12-24 14:06:19 +0530

    first ##restapi

--- src/main/java/com/sample/springapp/HelloWorldController.java
+++ src/main/java/com/sample/springapp/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String helloworld(){
-        return "Hello world Congrats you wrote first API!!";
+        return "Hello world Congrats you Wrote first API!!";
     }
 
 
