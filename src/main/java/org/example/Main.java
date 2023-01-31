379d2fa88ca99e6531f91e898a6972790e2319fd
Nikhil <Sknikhil2000@gmail.com>
2023-01-31 23:25:26 +0530

    31jan topics done breakpoint

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a =2;
+
+        System.out.println("Hello world!");
+        a=4;
+        System.out.println("my project started"+a);
+    }
+}
